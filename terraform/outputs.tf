ad81832ede667d04f97c91d1e333ba96ff89ea16
Mustafa Batuhan Büber <mustafa.buber@std.bogazici.edu.tr>
2025-12-13 19:08:29 +0300

    Implement Terraform outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,30 @@
+output "vpc_name" {
+  description = "The name of the VPC"
+  value       = google_compute_network.vpc.name
+}
+
+output "vm_internal_ip" {
+  description = "Internal IP of the PostgreSQL VM"
+  value       = google_compute_instance.postgres_vm.network_interface[0].network_ip
+}
+
+output "vm_public_ip" {
+  description = "Public IP of the PostgreSQL VM (Ephemeral)"
+  value       = google_compute_instance.postgres_vm.network_interface[0].access_config[0].nat_ip
+}
+
+output "cluster_name" {
+  description = "GKE Cluster Name"
+  value       = google_container_cluster.primary.name
+}
+
+output "cluster_endpoint" {
+  description = "GKE Cluster Endpoint"
+  value       = google_container_cluster.primary.endpoint
+  sensitive   = true
+}
+
+output "get_credentials_command" {
+  description = "Command to configure kubectl"
+  value       = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --zone ${var.zone} --project ${var.project_id}"
+}
